9d38b3e73e0ae3a27549b0938111e41e5539d2d0
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-10-30 16:46:14 -0400

    setup useDarkMode and useEffect hooks....I think?

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,16 @@
+import { useEffect } from "react";
+import { useLocalStorage } from "./useLocalStorage";
+import { brotliDecompress } from "zlib";
+
+export const useDarkMode = (key, initialValue) => {
+  const [colorScheme, setColorScheme] = useLocalStorage(key, initialValue);
+
+  useEffect(() => {
+    const body = document.querySelector(body);
+    window.localStorage.getItem(initialValue)
+      ? body.classList.add("dark-mode")
+      : body.classList.remove("dark-mode");
+  }, [colorScheme]);
+
+  return [colorScheme, setColorScheme];
+};
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -2,22 +2,14 @@ import { useState } from "react";
 
 export const useLocalStorage = (key, initialValue) => {
   const [storedValue, setStoredValue] = useState(() => {
-    //1.  make hook value, get
-
-    //declare variable with value equal to `key` paramater and JSON PARSE IT
     const item = window.localStorage.getItem(key);
-    //does item exist? if so, return it, otherwise return initialValue with is undefined by default
     return item ? JSON.parse(item) : initialValue;
   });
 
-  //2.  setter function, set
   const setValue = value => {
-    //set state according to parameter
     setStoredValue(value);
-    //save to localStorage IN JSON STRING FORMAT
     window.localStorage.setItem(key, JSON.stringify(value));
   };
 
-  //return state as `storedValue`
-  return [storedValue];
+  return [storedValue, setValue];
 };
