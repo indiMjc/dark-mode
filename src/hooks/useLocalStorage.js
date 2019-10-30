d228aa565a240207eb162bb979d5b24245504d7a
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-10-30 16:06:50 -0400

    created setter function for useLocalStorage hook

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -2,11 +2,21 @@ import { useState } from "react";
 
 export const useLocalStorage = (key, initialValue) => {
   const [storedValue, setStoredValue] = useState(() => {
-    //declare variable with value equal to `key` paramater and parse it to return it in JSON format
+    //1.  make hook value, get
+
+    //declare variable with value equal to `key` paramater and JSON PARSE IT
     const item = window.localStorage.getItem(key);
     //does item exist? if so, return it, otherwise return initialValue with is undefined by default
     return item ? JSON.parse(item) : initialValue;
   });
+
+  //2.  setter function, set
+  const setValue = value => {
+    //set state according to parameter
+    setStoredValue(value);
+    //save to localStorage IN JSON STRING FORMAT
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
 
   //return state as `storedValue`
   return [storedValue];
