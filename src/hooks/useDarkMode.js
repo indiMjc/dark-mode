3777603dacc056823850192ca8cc4cac9c1768f9
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-10-30 19:35:19 -0400

    removed useEffect hook, mvp met

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -2,15 +2,13 @@ import { useEffect } from "react";
 import { useLocalStorage } from "./useLocalStorage";
 import { brotliDecompress } from "zlib";
 
-export const useDarkMode = (key, initialValue) => {
-  const [colorScheme, setColorScheme] = useLocalStorage(key, initialValue);
+export const useDarkMode = initialValue => {
+  const [colorScheme, setColorScheme] = useLocalStorage(initialValue);
 
-  useEffect(() => {
-    const body = document.querySelector("body");
-    colorScheme
-      ? body.classList.add("dark-mode")
-      : body.classList.remove("dark-mode");
-  }, [colorScheme]);
+  const body = document.querySelector("body");
+  colorScheme
+    ? body.classList.add("dark-mode")
+    : body.classList.remove("dark-mode");
 
   return [colorScheme, setColorScheme];
 };
